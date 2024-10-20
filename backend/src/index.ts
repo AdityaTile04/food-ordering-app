aea428a9f9a50c2d5bbe378560961e82e07fc810
Aditya Tile <adityatile99@gmail.com>
2024-10-20 23:12:34 +0530

    add database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const port = 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(`Some Error in Database : ${err}`);
+  });
 
 const app = express();
 
